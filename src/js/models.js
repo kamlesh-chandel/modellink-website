1895fa58b308cfa24378d2caa8d43352f489fe50
kamlesh-chandel <kamlesh@gkmit.co>
2025-12-07 16:24:55 +0530

    feat: implemented edit model funtionality

--- src/js/models.js
+++ src/js/models.js
@@ -1,12 +1,24 @@
 const STORAGE_KEY = "modellink_models";
 const form = document.getElementById("modelForm");
 const modelsContainer = document.getElementById("modelsContainer");
+const submitBtn = document.getElementById("submitBtn");
 
-form.addEventListener("submit", createModel);
+let editModelId = null;
+
+form.addEventListener("submit", handleSubmit);
 document.addEventListener("DOMContentLoaded", renderModels);
 
-function createModel(e) {
+function handleSubmit(e) {
   e.preventDefault();
+
+  if (editModelId) {
+    updateModel();
+  } else {
+    createModel();
+  }
+}
+
+function createModel(e) {
   const models = getModels();
   const newModel = createNewModel();
   models.push(newModel);
@@ -20,8 +32,7 @@ function renderModels() {
   modelsContainer.innerHTML = "";
 
   if (models.length === 0) {
-    modelsContainer.innerHTML =
-      `<p class="no-models">No models added yet.</p>`;
+    modelsContainer.innerHTML = `<p class="no-models">No models added yet.</p>`;
     return;
   }
 
@@ -33,10 +44,53 @@ function renderModels() {
       <img src="${model.image}" alt="${model.name}" />
       <h3>${model.name}</h3>
       <p>${model.category}</p>
+      <button onclick="editModel(${model.id})" class="edit-btn">
+        Edit
+      </button>
       <button class="delete-btn" onclick="deleteModel(${model.id})">Delete</button>
     `;
     modelsContainer.appendChild(card);
   });
+}
+
+function editModel(id) {
+  const models = getModels();
+  const model = models.find((m) => m.id === id);
+
+  if (!model) return;
+
+  document.getElementById("modelName").value = model.name;
+  document.getElementById("modelCategory").value = model.category;
+  document.getElementById("modelImage").value = model.image;
+
+  editModelId = id;
+  submitBtn.textContent = "Update Model";
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+function updateModel() {
+  let models = getModels();
+
+  models = models.map((model) =>
+    model.id === editModelId
+      ? {
+          ...model,
+          name: document.getElementById("modelName").value.trim(),
+          category: document.getElementById("modelCategory").value.trim(),
+          image: document.getElementById("modelImage").value.trim(),
+        }
+      : model
+  );
+
+  saveModels(models);
+
+  editModelId = null;
+  submitBtn.textContent = "Create Model";
+  form.reset();
+  renderModels();
 }
 
 function deleteModel(id) {
@@ -59,7 +113,7 @@ function saveModels(models) {
 
 function createNewModel() {
   return {
-    id: getModels().length+1,
+    id: getModels().length + 1,
     name: document.getElementById("modelName").value.trim(),
     category: document.getElementById("modelCategory").value.trim(),
     image: document.getElementById("modelImage").value.trim(),
